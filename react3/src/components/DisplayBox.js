fd07be604d18953d9b65c243167305174ba91bf7
nolanjharris <thenolanjames@gmail.com>
2019-07-07 17:29:22 -0500

    refactor Edit so only DisplayBox holds state

--- react3/src/components/DisplayBox.js
+++ react3/src/components/DisplayBox.js
@@ -5,81 +5,129 @@ import Edit from './Edit';
 import data from '../data';
 
 class DisplayBox extends Component {
-    constructor() {
-        super();
+    state = {
+        data: data,
+        currentObjectIndex: 0,
+        addHidden: true,
+        editingObject: {},
+        editHidden: true
+    };
 
-        this.state = {
-            data: data,
-            currentObjectIndex: 0,
-            addHidden: true,
-            editHidden: true
-        }
-        this.handleNext = this.handleNext.bind(this);
-        this.handlePrevious = this.handlePrevious.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        this.handleNew = this.handleNew.bind(this);
-        this.handleAddObject = this.handleAddObject.bind(this);
-        this.handleEditObject = this.handleEditObject.bind(this);
-        this.handleEdit = this.handleEdit.bind(this);
-    }
-
-    handlePrevious() {
+    handlePrevious = () => {
         if (this.state.currentObjectIndex > 0) {
-            this.setState({ currentObjectIndex: this.state.currentObjectIndex - 1, editHidden: true, addHidden: true })
+            this.setState({
+                currentObjectIndex: this.state.currentObjectIndex - 1,
+                editHidden: true,
+                addHidden: true
+            });
         } else {
-            this.setState({ currentObjectIndex: this.state.data.length - 1, editHidden: true, addHidden: true })
+            this.setState({
+                currentObjectIndex: this.state.data.length - 1,
+                editHidden: true,
+                addHidden: true
+            });
         }
-    }
+    };
 
-    handleNext() {
+    handleNext = () => {
         if (this.state.currentObjectIndex < this.state.data.length - 1) {
-            this.setState({ currentObjectIndex: this.state.currentObjectIndex + 1, editHidden: true, addHidden: true })
+            this.setState({
+                currentObjectIndex: this.state.currentObjectIndex + 1,
+                editHidden: true,
+                addHidden: true
+            });
         } else {
-            this.setState({ currentObjectIndex: 0, editHidden: true, addHidden: true })
+            this.setState({
+                currentObjectIndex: 0,
+                editHidden: true,
+                addHidden: true
+            });
         }
-    }
+    };
 
-    handleNew() {
-        this.setState({ addHidden: false })
-    }
+    handleNew = () => {
+        this.setState({ addHidden: false });
+    };
 
-    handleEdit() {
-        this.setState({ editHidden: false })
-        console.log(this.state.currentObjectIndex);
-    }
+    handleEdit = () => {
+        this.setState({ editHidden: false, editingObject: this.state.data[this.state.currentObjectIndex] });
+    };
 
-    handleDelete() {
+    handleDelete = () => {
         let dataCopy = [...this.state.data];
         dataCopy.splice(this.state.currentObjectIndex, 1);
         let index;
-        if (this.state.data.length > 1 && this.state.currentObjectIndex === this.state.data.length - 1) {
+        if (
+            this.state.data.length > 1 &&
+            this.state.currentObjectIndex === this.state.data.length - 1
+        ) {
             index = this.state.currentObjectIndex - 1;
         } else {
             index = this.state.currentObjectIndex;
         }
         this.setState({ data: dataCopy, currentObjectIndex: index });
-    }
+    };
 
-    handleAddObject(newObject) {
+    handleAddObject = newObject => {
         this.setState({ data: [...this.state.data, newObject], addHidden: true });
-    }
+    };
 
-    handleEditObject(edittedObject) {
-        console.log(edittedObject)
-        let objectCopy = [...this.state.data];
-        objectCopy.splice(this.state.currentObjectIndex, 1, edittedObject)
-        this.setState({ data: objectCopy, editHidden: true });
+    handleEditObject = edittedObject => {
+        const newData = {
+            city: edittedObject.city
+                ? edittedObject.city
+                : this.state.editingObject.city,
+            country: edittedObject.country
+                ? edittedObject.country
+                : this.state.editingObject.country,
+            employer: edittedObject.employer
+                ? edittedObject.employer
+                : this.state.editingObject.employer,
+            favoriteMovies: [
+                edittedObject.movie1
+                    ? edittedObject.movie1
+                    : this.state.editingObject.favoriteMovies[0],
+                edittedObject.movie2
+                    ? edittedObject.movie2
+                    : this.state.editingObject.favoriteMovies[1],
+                edittedObject.movie3
+                    ? edittedObject.movie3
+                    : this.state.editingObject.favoriteMovies[2]
+            ],
+            id: edittedObject.id ? edittedObject.id : 1,
+            name: {
+                first: edittedObject.firstName
+                    ? edittedObject.firstName
+                    : this.state.editingObject.name.first,
+                last: edittedObject.lastName
+                    ? edittedObject.lastName
+                    : this.state.editingObject.name.last
+            },
+            title: edittedObject.title
+                ? edittedObject.title
+                : this.state.editingObject.title
+        };
+        this.setState({ editingObject: newData });
+    };
 
-    }
+    handleSubmit = () => {
+        let dataCopy = [...this.state.data];
+        dataCopy.splice(this.state.currentObjectIndex, 1, this.state.editingObject);
+        this.setState({ data: dataCopy, editHidden: true, editingObject: this.state.data[this.state.currentObjectIndex] });
+    };
 
     render() {
-
         return (
             <div className="displayAll">
                 <div className="displayBox">
                     <div className="displayData">
-                        <DisplayData dataObject={this.state.data} dataIndex={this.state.currentObjectIndex} />
-                        <h1>{this.state.currentObjectIndex + 1}/{this.state.data.length}</h1>
+                        <DisplayData
+                            dataObject={this.state.data}
+                            dataIndex={this.state.currentObjectIndex}
+                        />
+                        <h1>
+                            {this.state.currentObjectIndex + 1}/{this.state.data.length}
+                        </h1>
                     </div>
                     <div className="buttonsDiv">
                         <button onClick={this.handlePrevious}>&lt; Previous</button>
@@ -91,23 +139,21 @@ class DisplayBox extends Component {
                         <button onClick={this.handleNext}>Next &gt;</button>
                     </div>
                 </div>
-                <New addObject={this.handleAddObject} addHidden={this.state.addHidden} />
-                {this.state.editHidden ? null :
+                <New
+                    addObject={this.handleAddObject}
+                    addHidden={this.state.addHidden}
+                />
+                {this.state.editHidden ? null : (
                     <Edit
-                        firstName={this.state.data[this.state.currentObjectIndex].name.first}
-                        lastName={this.state.data[this.state.currentObjectIndex].name.last}
-                        city={this.state.data[this.state.currentObjectIndex].city}
-                        country={this.state.data[this.state.currentObjectIndex].country}
-                        title={this.state.data[this.state.currentObjectIndex].title}
-                        employer={this.state.data[this.state.currentObjectIndex].employer}
-                        movie1={this.state.data[this.state.currentObjectIndex].favoriteMovies[0]}
-                        movie2={this.state.data[this.state.currentObjectIndex].favoriteMovies[1]}
-                        movie3={this.state.data[this.state.currentObjectIndex].favoriteMovies[2]}
                         index={this.state.currentObjectIndex}
+                        currentObject={this.state.data[this.state.currentObjectIndex]}
                         editHidden={this.state.editHidden}
-                        editObject={this.handleEditObject} />}
+                        editObject={this.handleEditObject}
+                        submitEdittedObject={this.handleSubmit}
+                    />
+                )}
             </div>
-        )
+        );
     }
 }
 
--- react3/src/components/Edit.js
+++ react3/src/components/Edit.js
@@ -1,139 +1,73 @@
 import React, { Component } from 'react';
 
 class Edit extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: this.props.firstName,
-            lastName: this.props.lastName,
-            city: this.props.city,
-            country: this.props.country,
-            title: this.props.title,
-            employer: this.props.employer,
-            movie1: this.props.movie1,
-            movie2: this.props.movie2,
-            movie3: this.props.movie3
-        }
-        console.log(this.state);
-        this.handleEditCity = this.handleEditCity.bind(this);
-        this.handleEditCountry = this.handleEditCountry.bind(this);
-        this.handleEditFirstName = this.handleEditFirstName.bind(this);
-        this.handleEditLastName = this.handleEditLastName.bind(this);
-        this.handleEditTitle = this.handleEditTitle.bind(this);
-        this.handleEditEmployer = this.handleEditEmployer.bind(this);
-        this.handleEditMovie1 = this.handleEditMovie1.bind(this);
-        this.handleEditMovie2 = this.handleEditMovie2.bind(this);
-        this.handleEditMovie3 = this.handleEditMovie3.bind(this);
-    }
-
-    componentDidUpdate() {
-        this.setState({
-            firstName: this.props.firstName,
-            lastName: this.props.lastName,
-            city: this.props.city,
-            country: this.props.country,
-            title: this.props.title,
-            employer: this.props.employer,
-            movie1: this.props.movie1,
-            movie2: this.props.movie2,
-            movie3: this.props.movie3
-        })
-    }
-
-    handleEditFirstName(e) {
-        this.setState({ firstName: e.target.value })
-    }
-
-    handleEditLastName(e) {
-        this.setState({ lastName: e.target.value })
-    }
-
-    handleEditCity(e) {
-        this.setState({ city: e.target.value })
-    }
-
-    handleEditCountry(e) {
-        this.setState({ country: e.target.value })
-    }
-
-    handleEditTitle(e) {
-        this.setState({ title: e.target.value })
-    }
-
-    handleEditEmployer(e) {
-        this.setState({ employer: e.target.value })
-    }
-
-    handleEditMovie1(e) {
-        this.setState({
-            movie1: e.target.value
-        })
-    }
-
-    handleEditMovie2(e) {
-        this.setState({
-            movie2: e.target.value
-        })
-    }
-
-    handleEditMovie3(e) {
-        this.setState({
-            movie3: e.target.value
-        })
-    }
-
-    editObject() {
-        const { firstName, lastName, city, country, title, employer } = this.state;
-        console.log({ firstName, lastName, city, country, title, employer });
-        // let firstName = this.state.firstName;
-        // let lastName = this.state.lastName;
-        // let city = this.state.city;
-        // let country = this.state.country;
-        // let title = this.state.title;
-        // let employer = this.state.employer;
-        let favoriteMovies = [this.state.movie1, this.state.movie2, this.state.movie3];
-        let edittedObject = {
-            name: { first: firstName, last: lastName },
-            city: city,
-            country: country,
-            title: title,
-            employer: employer,
-            favoriteMovies: favoriteMovies
-        }
-        this.props.editObject(edittedObject)
-        console.log(this.props.index);
-
-    }
+    handleChange = e => {
+        this.props.editObject({ [e.target.name]: e.target.value });
+    };
 
     render() {
-
         return (
             <>
-
                 <div className="promptFields">
                     <div className="addObject prompts">
                         <p>First Name: </p>
-                        <input ref='firstName' defaultValue={this.props.firstName} onChange={this.handleEditFirstName} />
+                        <input
+                            name="firstName"
+                            defaultValue={this.props.currentObject.name.first}
+                            onChange={this.handleChange}
+                        />
                         <p>Last Name: </p>
-                        <input defaultValue={this.props.lastName} onChange={this.handleEditLastName} />
+                        <input
+                            name="lastName"
+                            defaultValue={this.props.currentObject.name.last}
+                            onChange={this.handleChange}
+                        />
                         <p>Hometown: </p>
-                        <input defaultValue={this.props.city} onChange={this.handleEditCity} />
+                        <input
+                            name="city"
+                            defaultValue={this.props.currentObject.city}
+                            onChange={this.handleChange}
+                        />
                         <p>Country: </p>
-                        <input defaultValue={this.props.country} onChange={this.handleEditCountry} />
+                        <input
+                            name="country"
+                            defaultValue={this.props.currentObject.country}
+                            onChange={this.handleChange}
+                        />
                         <p>Job Title: </p>
-                        <input defaultValue={this.props.title} onChange={this.handleEditTitle} />
+                        <input
+                            name="title"
+                            defaultValue={this.props.currentObject.title}
+                            onChange={this.handleChange}
+                        />
                         <p>Employer: </p>
-                        <input defaultValue={this.props.employer} onChange={this.handleEditEmployer} />
+                        <input
+                            name="employer"
+                            defaultValue={this.props.currentObject.employer}
+                            onChange={this.handleChange}
+                        />
                         <p>Favorite Movies: </p>
-                        <input defaultValue={this.props.movie1} onChange={this.handleEditMovie1} />
-                        <input defaultValue={this.props.movie2} onChange={this.handleEditMovie2} />
-                        <input defaultValue={this.props.movie3} onChange={this.handleEditMovie3} />
-                        <button onClick={this.editObject.bind(this)}>Edit Item</button>
+                        <input
+                            name="movie1"
+                            defaultValue={this.props.currentObject.favoriteMovies[0]}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            name="movie2"
+                            defaultValue={this.props.currentObject.favoriteMovies[1]}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            name="movie3"
+                            defaultValue={this.props.currentObject.favoriteMovies[2]}
+                            onChange={this.handleChange}
+                        />
+                        <button onClick={this.props.submitEdittedObject}>Edit Item</button>
                     </div>
                 </div>
                 }
-            </>
-        )
+      </>
+        );
     }
 }
 
